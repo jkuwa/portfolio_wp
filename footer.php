22417587c9197e9bd869dd8da15ef7636ae99112
kuwa <163106848+jkuwa@users.noreply.github.com>
2024-11-26 15:38:29 +0900

    追加：フッターSNSボタン

--- footer.php
+++ footer.php
@@ -1,8 +1,47 @@
   <!-- FOOTER -->
   <footer class="l-footer p-footer js-footer">
-    <ul class="c-outline--list p-footer_sns">
-      <li><a href="#" class="c-icon__x -footer">X</a></li>
-      <li><a href="#" class="c-icon__github -footer">GitHub</a></li>
+
+    <!-- SNS MENU -->
+    <?php 
+      $x_url = get_theme_mod('my_portfolio_x_url');
+      $facebook_url = get_theme_mod('my_portfolio_facebook_url');
+      $instagram_url = get_theme_mod('my_portfolio_instagram_url');
+      $github_url = get_theme_mod('my_portfolio_github_url');
+    ?>
+
+    <ul class="c-outline--list p-footer__sns">
+      <?php if ($x_url): ?>
+        <li>
+          <a href="<?php echo esc_url($x_url); ?>" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-x-twitter" aria-hidden="true"></i><span>X</span>
+          </a>
+        </li>
+      <?php endif; ?>
+
+      <?php if ($facebook_url): ?>
+        <li>
+          <a href="<?php echo esc_url($facebook_url); ?>" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-facebook" aria-hidden="true"></i><span>Facebook</span>
+          </a>
+        </li>
+      <?php endif; ?>
+
+      <?php if ($instagram_url): ?>
+        <li>
+          <a href="<?php echo esc_url($instagram_url); ?>" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-instagram" aria-hidden="true"></i><span>Instagram</span>
+          </a>
+        </li>
+      <?php endif; ?>
+
+      <?php if ($github_url): ?>
+        <li>
+          <a href="<?php echo esc_url($github_url); ?>" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-github" aria-hidden="true"></i><span>GitHub</span>
+          </a>
+        </li>
+      <?php endif; ?>
+
     </ul>
     <small>Copyright &copy; kuwa junna All Rights Reserved.</small>
   </footer>
