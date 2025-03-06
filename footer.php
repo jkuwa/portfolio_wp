e196093237c9ca0e562b981d290570a5c0c6779d
kuwa <163106848+jkuwa@users.noreply.github.com>
2025-03-06 17:18:37 +0900

    修正：reCAPTCHA非表示

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
     <ul class="c-outline--list p-footer__sns">
       <?php get_template_part('template/sns'); ?>
     </ul>
+    <p class="p-footer__reCaptcha">このサイトはreCAPTCHAによって保護されており、<br>Googleの <a href="https://policies.google.com/privacy" target="_blank">プライバシーポリシー</a> と <a href="https://policies.google.com/terms" target="_blank">利用規約</a> が適用されます。</p>
     <small>Copyright &copy; kuwa junna All Rights Reserved.</small>
   </footer>
 
